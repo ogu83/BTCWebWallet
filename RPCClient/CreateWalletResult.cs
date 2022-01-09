94d585990472571d907538a8ad1525397b590626
oguzkoroglu <oguzkoroglu@gmail.com>
2022-01-09 03:11:14 +0300

    Create Wallet Result to file

---
+++ RPCClient/CreateWalletResult.cs
@@ -0,0 +1,8 @@
+namespace BTCWebWallet.RPCClient;
+
+public class CreateWalletResult 
+{
+    public string Name { get; set; }
+    
+    public string Warning { get; set; }
+}
--- RPCClient/ListWalletsResult.cs
+++ RPCClient/ListWalletsResult.cs
@@ -4,10 +4,3 @@ public class ListWalletsResult : List<string>
 {
 
 }
-
-public class CreateWalletResult 
-{
-    public string Name { get; set; }
-    
-    public string Warning { get; set; }
-}
