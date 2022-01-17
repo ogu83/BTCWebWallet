027853f34a0527e31e76ad0a9f52ac2ac28a64c4
Oguz Koroglu <Oguz.Koroglu@haibooks.com>
2022-01-17 15:15:04 +0300

    Working On Error Views

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,13 +1,26 @@
+using System.Text;
+using BTCWebWallet.Models;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BTCWebWallet.Controllers;
 
-public class BaseController : Controller 
+public class BaseController : Controller
 {
     protected readonly string rpc_id = $"BTCWebWallet_{Guid.NewGuid().ToString()}";
 
-    public IActionResult SetCulture(string id="en")
+    private readonly ISession? session;
+
+    public BaseController(IHttpContextAccessor httpContextAccessor)
+    {
+        if (httpContextAccessor.HttpContext != null) 
+        {
+            this.session = httpContextAccessor.HttpContext.Session;
+        }        
+    }
+
+    public IActionResult SetCulture(string id = "en")
     {
         string culture = id;
         Response.Cookies.Append(
@@ -31,5 +44,62 @@ public class BaseController : Controller
         );
 
         return LocalRedirect(returnUrl);
+    }   
+
+    protected void SetSession<T>(string key, T obj) where T : class
+    {
+        var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj));
+        HttpContext.Session.Set(key, bytes);
+    }
+
+    protected T? GetSession<T>(string key) where T : class
+    {
+        var bytes = HttpContext.Session.Get(key);
+        if (bytes != null) 
+        {
+            var json = Encoding.UTF8.GetString(bytes);
+            T? obj = JsonConvert.DeserializeObject<T>(json);
+            return obj;
+        }
+        else 
+        {
+            return null;
+        }
+    }
+
+    public void AddPageError(ErrorViewModel model)
+    {
+        
+        var errors = GetSession<List<ErrorViewModel>>("Errors") ?? new List<ErrorViewModel>();
+        errors.Add(model);
+        SetSession<List<ErrorViewModel>>("Errors", errors);
+    }
+
+    public void AddPageError(List<ErrorViewModel> model)
+    {
+        var errors = GetSession<List<ErrorViewModel>>("Errors") ?? new List<ErrorViewModel>();
+        errors.AddRange(model);
+        SetSession<List<ErrorViewModel>>("Errors", errors);
+    }
+
+    public void ClearPageError()
+    {
+        var errors = GetSession<List<ErrorViewModel>>("Errors") ?? new List<ErrorViewModel>();
+        errors.Clear();
+        SetSession<List<ErrorViewModel>>("Errors", errors);
+    }
+
+    [HttpPost]
+    public IActionResult RemovePageError(int code)
+    {
+        var errors = GetSession<List<ErrorViewModel>>("Errors") ?? new List<ErrorViewModel>();
+        var error = errors.FirstOrDefault(x => x.Code == code);
+        if (error != null)
+        {
+            errors.Remove(error);
+            SetSession<List<ErrorViewModel>>("Errors", errors);
+        }
+
+        return new JsonResult(true);
     }
 }
